0d6d47528098b39ac07e86569f55082a41a7cc1b
Orghaniian <julien.boulvert@hotmail.fr>
2023-01-10 23:22:51 +0100

    push update css

--- ChoixAstreIPS/src/main.ts
+++ ChoixAstreIPS/src/main.ts
@@ -1,7 +1,5 @@
 import { enableProdMode } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppComponent } from "./app/app.component";
 import { AppModule } from "./app/app.module";
 
 import { environment } from "./environments/environment";
@@ -9,7 +7,5 @@ import { environment } from "./environments/environment";
 if (environment.production) {
   enableProdMode();
 }
-
-//bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
