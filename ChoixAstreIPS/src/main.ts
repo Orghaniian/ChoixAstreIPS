33654959c31cf90635fd5904b2a301b050a29c88
Orghaniian <julien.boulvert@hotmail.fr>
2023-01-12 21:30:53 +0100

    set window minimum size

--- ChoixAstreIPS/src/main.ts
+++ ChoixAstreIPS/src/main.ts
@@ -3,9 +3,13 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 
 import { environment } from "./environments/environment";
+import {appWindow, LogicalSize} from "@tauri-apps/api/window";
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+
+// noinspection JSIgnoredPromiseFromCall
+appWindow.setMinSize(new LogicalSize(940, 400));
