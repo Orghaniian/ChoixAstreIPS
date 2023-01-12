6bfbfc870e4782506283e75766c52ee58358b847
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2023-01-12 21:51:13 +0100

    effectif

--- ChoixAstreIPS/src/app/proba-bar-all-students/proba-bar-all-students.component.ts
+++ ChoixAstreIPS/src/app/proba-bar-all-students/proba-bar-all-students.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Indicator, Weight } from 'src/Indicator';
 
 @Component({
   selector: 'app-proba-bar-all-students',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./proba-bar-all-students.component.scss']
 })
 export class ProbaBarAllStudentsComponent {
+
+  @Input() studentsIndicators!: Indicator[];
+  @Input() indicatorsWeights!: Weight[];
+  public ipsAmount!: number;
+  public astreAmount!: number;
+
+  ngAfterContentInit(): void {
+    this.calculateAmountIPSandASTRE();
+  }
+
+  public calculateAmountIPSandASTRE(): void {
+    let tempIpsAmount = 0;
+    let tempAstreAmount = 0;
+    this.studentsIndicators.forEach((studentIndication: Indicator) => {
+      if(studentIndication.computeProbability(this.indicatorsWeights) > 0) {
+        tempIpsAmount++;
+      } else if (studentIndication.computeProbability(this.indicatorsWeights) < 0) {
+        tempAstreAmount++;
+      }
+    });
+    this.ipsAmount = tempIpsAmount;
+    this.astreAmount = tempAstreAmount;
+    console.log(tempAstreAmount, tempIpsAmount)
+  }
 
 }
